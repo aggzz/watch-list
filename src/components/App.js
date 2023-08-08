0f088f2e3a9e92426847ac0c1c617139e1c7d031
agz <aggigeothy@gmail.com>
2023-08-08 18:13:34 +0000

    Update App.js and gridView.jsx

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import GridView from "./grid-view";
 import fetchMovieList from "../api/apiUtils";
 import { createUseStyles } from "react-jss";
+import { useMemo } from "react/cjs/react.production.min";
 
 const useStyles = createUseStyles({
   root: {
@@ -9,18 +10,43 @@ const useStyles = createUseStyles({
   },
 });
 
-const movieList = [{ title: "Johny", id: "aae" }];
+// const movieList = [{ title: "Johny", id: "aae" }];
 
 const App = () => {
   const classes = useStyles();
+  const [movieList, setMovieList] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [ totalCount, setTotalCount ] = useState(null);
+  const [ searchResult, setSearchResult ] = useState([]);
+  const [searchText, setSearchText] = useState('');
+
 
   useEffect(() => {
     fetchMovieList();
   }, []);
 
+  const onScroll = (pageNum) => {
+    fetchMovieList({ pageNumber: pageNum });
+  };
+
+  const handleSearch = (value) => {
+     setSearchText(value);
+  }
+
+  const getSearchResult = useMemo(() => {
+     const searchResult = movieList.filter((movieObj) => movieObject
+     .name
+     .toLowerCase()
+     .includes(searchText.toLowerCase()))
+  }, [searchText])
+
   return (
     <div className={classes.root}>
-      <GridView data={movieList} />
+      <SearchBox onSearch={handleSearch} onClear={} />
+      <GridView 
+        data={searchText ? getSearchResult(): movieList} 
+        onScrollEnd={} 
+      />
     </div>
   );
 };
--- src/components/grid-view/gridView.jsx
+++ src/components/grid-view/gridView.jsx
@@ -7,7 +7,12 @@ const GridView = (props) => {
   return (
     <div>
       {data.map((dataObj) => (
-        <div key={dataObj.id}>{dataObj.title}</div>
+        <div key={dataObj.id}>
+          <img
+            src={`https://test.create.diagnal.com/images/${dataObj.posterImage}`}
+          />
+          {dataObj.title}
+        </div>
       ))}
     </div>
   );
