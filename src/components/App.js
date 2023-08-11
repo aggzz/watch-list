55c330bfb9404a70c413db1141ae2755e8300f02
agz <aggigeothy@gmail.com>
2023-08-11 01:49:11 +0000

    Update 8 files

--- src/components/App.js
+++ src/components/App.js
@@ -1,61 +1,96 @@
 import React, { useEffect, useState, useMemo, useRef } from "react";
 import GridView from "./grid-view";
 import SearchBox from "./searchbox";
-import './style.css';
+import "./style.css";
 
 const App = () => {
-  const classes = useStyles();
   const [movieList, setMovieList] = useState([]);
   const [pageNumber, setPageNumber] = useState(0);
-  const [ totalCount, setTotalCount ] = useState(null);
-  const [ searchResult, setSearchResult ] = useState([]);
-  const [searchText, setSearchText] = useState('');
-  const [ isResultLoading, setIsResultLoading ] = useState(false);
-  const [ error, setError ] = useState(null);
-
-  const fetchMovieList = async({ pageno }) => {
+  const [totalCount, setTotalCount] = useState(null);
+  const [searchText, setSearchText] = useState("");
+  const [isResultLoading, setIsResultLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const observerTarget = useRef(null);
+  const fetchMovieList = async ({ pageno }) => {
     setIsResultLoading(true);
-    let response = await fetch(`https://test.create.diagnal.com/data/pages${pageno}.json`);
+    let response = await fetch(
+      `https://test.create.diagnal.com/data/page${pageno}.json`
+    );
     try {
-      if(response.status === 200 ){
+      if (response.status === 200) {
         response = await response.json();
-        const newList = movieList.concat(response?.page?.[`content-items`]?.content || []);
+        const newList = movieList.concat(
+          response.page[`content-items`].content || []
+        );
         setMovieList(newList);
-        setTotalCount(response?.page?.[`total-content-items`]);
+        setTotalCount(response.page[`total-content-items`]);
+        setPageNumber(pageno);
+      } else {
+        //sth
+        setError("Unable to load data");
       }
-    } catch(error) {
-
-      
+    } catch (error) {
+      setError("Unable to load data");
+    } finally {
+      setIsResultLoading(false);
     }
-  }
-
-
-  useEffect(() => {
-    fetchMovieList();
-  }, []);
-
-  const onScroll = (pageNum) => {
-    fetchMovieList({ pageNumber: pageNum });
   };
 
+  useEffect(() => {
+    fetchMovieList({ pageno: 1 });
+  }, []);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (enteries) => {
+        if (
+          enteries[0].isIntersecting &&
+          (movieList.length < totalCount || totalCount === null)
+        ) {
+          console.log("dsd");
+          fetchMovieList({ pageno: pageNumber + 1 });
+        }
+      },
+      { threshold: 1 }
+    );
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+    }
+    return () => {
+      if (observerTarget.current) {
+        observer.unobserve(observerTarget.current);
+      }
+    };
+  }, [observerTarget]);
+
   const handleSearch = (value) => {
-     setSearchText(value);
-  }
+    setSearchText(value);
+  };
 
   const getSearchResult = useMemo(() => {
-     const searchResult = movieList.filter((movieObj) => movieObject
-     .name
-     .toLowerCase()
-     .includes(searchText.toLowerCase()))
-  }, [searchText])
+    const searchResult = movieList.filter((movieObj) =>
+      movieObj.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    return searchResult;
+  }, [searchText]);
 
   return (
-    <div className={classes.root}>
-      <SearchBox onSearch={handleSearch} onClear={} />
-      <GridView 
-        data={searchText ? getSearchResult(): movieList} 
-        onScrollEnd={} 
-      />
+    <div className="movielist-container">
+      <div className="header">
+        <div>
+          <img
+            src="https://test.create.diagnal.com/images/Back.png"
+            alt="back-arrow"
+            width="20px"
+          />
+          <span className="header-text"> Romantic Comedy </span>
+        </div>
+        <SearchBox onSearch={handleSearch} />
+      </div>
+      <GridView data={searchText ? getSearchResult : movieList} />
+      {isResultLoading && <p> Loading </p>}
+      <div ref={observerTarget} />
     </div>
   );
 };
--- src/components/grid-view/gridView.jsx
+++ src/components/grid-view/gridView.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./style.css";
 
 const GridView = (props) => {
   const { data } = props;
+  const gridContainerRef = useRef();
 
   return (
-    <div>
+    <div className="grid-wrapper" ref={gridContainerRef}>
       {data.map((dataObj) => (
-        <div key={dataObj.id}>
+        <div
+          key={dataObj.id}
+          className="grid-item"
+          style={{ maxWidth: (gridContainerRef.current.offsetWidth - 40) / 3 }}
+        >
           <img
-            src={`https://test.create.diagnal.com/images/${dataObj.posterImage}`}
+            src={`https://test.create.diagnal.com/images/${
+              dataObj[`poster-image`]
+            }`}
+            className="grid-image"
           />
-          {dataObj.title}
+          <div>{dataObj.name} </div>
         </div>
       ))}
     </div>
